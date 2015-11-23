394e09d8a73055fc11c301ba88858a1d64d263c4
Nitya Dhanushkodi <nitya.dhanushkodi@students.olin.edu>
2015-11-23 00:01:49 -0500

    commented path find

--- path_find.py
+++ path_find.py
@@ -7,31 +7,40 @@ graph = {'A': ['B', 'C'],
 
 def find_path(graph, start, end, path=[]):
         path = path + [start]
+
+        #base case
         if start == end:
             return path
+
+        #check if start is in the graph
         if not graph.has_key(start):
-            return []
+            return None
+
+        #for each child
         for node in graph[start]:
             if node not in path:
+                #find the child's path
                 newpath = find_path(graph, node, end, path)
                 if newpath: return newpath
-        return []
+        return None
 
-def find_shortest_path(graph, start, end):
-        path = []
+def find_shortest_path(graph, start, end, path=[]):
         path = path + [start]
 
+        #base case
         if start == end:
             return path
 
-        if graph.has_key(start) == False:
-            return []
-
-        shortest = []
+        #check if start is in graph
+        if not graph.has_key(start):
+            return None
+        shortest = None
         for node in graph[start]:
             if node not in path:
+                #find the child's path until base case
                 newpath = find_shortest_path(graph, node, end, path)
                 if newpath:
+                    #extra check for making sure shortest path
                     if not shortest or len(newpath) < len(shortest):
                         shortest = newpath
         return shortest
