93eb5d2a4a22afeb92e4423e8f9d27f4bd1121b9
Kiki <shivali.chandra@students.olin.edu>
2015-12-12 15:49:51 -0500

    Cleaned code, push outside while loop

--- bidirectional.c
+++ bidirectional.c
@@ -26,53 +26,51 @@ int main(int argc, char **argv) {
         }
         exit(1);
     }
-
-    redisReply *all_keys;
-    all_keys = redisCommand(c, "KEYS *");
     
-    Node *start_queue;
-    Node *end_queue;
-    hashtable_t *hashtable_start = ht_create( 65536 );
-    hashtable_t *hashtable_end = ht_create( 65536 );
-    hashtable_t *hashtable_parents_start = ht_create( 65536 );
-    hashtable_t *hashtable_parents_end = ht_create( 65536 );
     /*char *start = argv[1];
     char *end = argv[2];*/
     char *start = "0000147";
     char *end = "0000012";
     
+    Node *start_queue;
     start_queue = make_node(start, NULL);
+    
+    Node *end_queue;
     end_queue = make_node(end, NULL);
+    
+    Node *path_to_meet_s;
+    path_to_meet_s = make_node(start, NULL);
+    
+    Node *path_to_meet_e; 
+    path_to_meet_e = make_node(end, NULL);
+    
+    hashtable_t *hashtable_start = ht_create( 65536 );
+    hashtable_t *hashtable_end = ht_create( 65536 );
+    
+    hashtable_t *hashtable_parents_start = ht_create( 65536 );
+    hashtable_t *hashtable_parents_end = ht_create( 65536 );
+       
     char *start_name = start;
     char *end_name = end;
-    Node *path_to_meet_s;
-    Node *path_to_meet_e;
-    path_to_meet_s = make_node(start, NULL);
-    path_to_meet_e = make_node(end, NULL);
-    char *met1 = start_name;
-    char *met2 = end_name;
         
     while ((strcmp(start_name, "NULL") != 0) && (strcmp(end_name, "NULL") != 0)) {
         start_name = peek(&start_queue);
 		end_name = peek(&end_queue);
+		
 		char *visited_in_end = ht_get(hashtable_end, start_name);
 		char *visited_in_start = ht_get(hashtable_start, end_name);
+		
 		if (strcmp(visited_in_end, "NULL") != 0) {
 			printf("met at %s \n", start_name);
 			end_name = start_name;
-			push_begin(&path_to_meet_s, start_name);
-			push_begin(&path_to_meet_e, end_name);
 			break;
 		}
+		
 		if (strcmp(visited_in_start, "NULL") != 0) {
 			printf("met at %s \n", end_name);
 			start_name = end_name;
-			push_begin(&path_to_meet_s, start_name);
-			push_begin(&path_to_meet_e, end_name);
 			break;
 		}
-		pop(&path_to_meet_s);
-		pop(&path_to_meet_e);
 		
 		char *check_start = ht_get(hashtable_start, start_name);
 		char *check_end = ht_get(hashtable_end, end_name);
@@ -112,13 +110,9 @@ int main(int argc, char **argv) {
 				}
 			}
 			ht_set(hashtable_end, end_name, "visited");
-			//ht_set(hashtable_end, end_name, "visited");
 		} //else { printf("already visited, skipping\n"); }
 		
 		if (strcmp(start_name, end_name) != 0) {
-			//printf("popping start %s end %s\n", peek(&start_queue), peek(&end_queue));
-			push_begin(&path_to_meet_s, start_name);
-			push_begin(&path_to_meet_e, end_name);
 			pop(&start_queue);
 			pop(&end_queue);
 		} else { 
@@ -126,29 +120,40 @@ int main(int argc, char **argv) {
 			break; 
 		}
     }
+    
+    push_begin(&path_to_meet_s, start_name);
+	push_begin(&path_to_meet_e, end_name);
+    
     char *start_parent = peek(&path_to_meet_s);
     char *end_parent = peek(&path_to_meet_e);
     
 	reverse(&path_to_meet_s);
 	pop(&path_to_meet_s);
+	
 	reverse(&path_to_meet_e);
 	pop(&path_to_meet_e);
 	
 	char *parent_st = ht_get(hashtable_parents_start, start_parent);
 	char *parent_en = ht_get(hashtable_parents_end, end_parent);
+	
 	while (strcmp(parent_st, start) != 0) {
 		push(&path_to_meet_s, parent_st);
 		parent_st = ht_get(hashtable_parents_start, parent_st);
 	}
+	
 	while (strcmp(parent_en, end) != 0) {
 		push(&path_to_meet_e, parent_en);
 		parent_en = ht_get(hashtable_parents_end, parent_en);
 	}
+	
 	push(&path_to_meet_s, parent_st);
 	reverse(&path_to_meet_s);
+	
 	push(&path_to_meet_e, parent_en);
 	pop(&path_to_meet_e);
+	
 	print_list(path_to_meet_s);
+	printf("\n");
 	print_list(path_to_meet_e);
 		
     /* Disconnects and frees the context */
@@ -156,4 +161,3 @@ int main(int argc, char **argv) {
 
     return 0;
 }
-
