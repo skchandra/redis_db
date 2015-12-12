adb36f2888b143138bd6005d6f7cba9e55f19095
Kiki <shivali.chandra@students.olin.edu>
2015-12-12 15:10:27 -0500

    Working bidirectional except for parent-child recognition

--- bidirectional.c
+++ bidirectional.c
@@ -38,7 +38,7 @@ int main(int argc, char **argv) {
     hashtable_t *hashtable_parents_end = ht_create( 65536 );
     /*char *start = argv[1];
     char *end = argv[2];*/
-    char *start = "0000138";
+    char *start = "0000147";
     char *end = "0000012";
     
     start_queue = make_node(start, NULL);
@@ -52,41 +52,32 @@ int main(int argc, char **argv) {
     char *met1 = start_name;
     char *met2 = end_name;
         
-    //while (strcmp(start_name, end_name) != 4389605843690) {
     while ((strcmp(start_name, "NULL") != 0) && (strcmp(end_name, "NULL") != 0)) {
         start_name = peek(&start_queue);
 		end_name = peek(&end_queue);
-		printf("start %i %s, end %i %s\n", strcmp(start_name, end), start_name, strcmp(start, end_name), end_name);
 		char *visited_in_end = ht_get(hashtable_end, start_name);
 		char *visited_in_start = ht_get(hashtable_start, end_name);
-		printf("%s %i %s %i\n", start_name, (strcmp(visited_in_end, "NULL")), end_name, (strcmp(visited_in_start, "NULL")));
 		if (strcmp(visited_in_end, "NULL") != 0) {
-			printf("met at %s", start_name);
+			printf("met at %s \n", start_name);
 			end_name = start_name;
-			push(&path_to_meet_s, start_name);
-			push(&path_to_meet_e, end_name);
+			push_begin(&path_to_meet_s, start_name);
+			push_begin(&path_to_meet_e, end_name);
 			break;
 		}
 		if (strcmp(visited_in_start, "NULL") != 0) {
-			printf("met at %s", end_name);
+			printf("met at %s \n", end_name);
 			start_name = end_name;
-			push(&path_to_meet_s, start_name);
-			push(&path_to_meet_e, end_name);
+			push_begin(&path_to_meet_s, start_name);
+			push_begin(&path_to_meet_e, end_name);
 			break;
 		}
-		push(&path_to_meet_s, start_name);
-		push(&path_to_meet_e, end_name);
+		pop(&path_to_meet_s);
+		pop(&path_to_meet_e);
 		
 		char *check_start = ht_get(hashtable_start, start_name);
 		char *check_end = ht_get(hashtable_end, end_name);
-		//char *check_end = ht_get(hashtable_end, end_name);
 		
 		if(strcmp(check_start, "NULL")==0){	
-			/*if (strcmp(end_name, start_name) == 0) {
-				met1 = start_name;
-				met2 = end_name;
-				break;
-			} */
 			char word_reply1[100] = "";
 			strcat(word_reply1, "LRANGE ");
 			strcat(word_reply1, start_name);
@@ -103,14 +94,9 @@ int main(int argc, char **argv) {
 			}
 			
 			ht_set(hashtable_start, start_name, "visited");
-		} else { printf("already visited, skipping"); }
+		} //else { printf("already visited, skipping"); }
         
         if(strcmp(check_end, "NULL")==0){	
-			/*if (strcmp(end_name, start_name) == 0) {
-				met1 = start_name;
-				met2 = end_name;
-				break;
-			} */
 			char word_reply2[100] = "";
 			strcat(word_reply2, "LRANGE ");
 			strcat(word_reply2, end_name);
@@ -127,31 +113,26 @@ int main(int argc, char **argv) {
 			}
 			ht_set(hashtable_end, end_name, "visited");
 			//ht_set(hashtable_end, end_name, "visited");
-		} else { printf("already visited, skipping\n"); }
+		} //else { printf("already visited, skipping\n"); }
 		
 		if (strcmp(start_name, end_name) != 0) {
-			printf("popping start %s end %s\n", peek(&start_queue), peek(&end_queue));
+			//printf("popping start %s end %s\n", peek(&start_queue), peek(&end_queue));
+			push_begin(&path_to_meet_s, start_name);
+			push_begin(&path_to_meet_e, end_name);
 			pop(&start_queue);
 			pop(&end_queue);
 		} else { 
-			printf("met %s %s", start_name, end_name);
+			printf("met %s %s \n", start_name, end_name);
 			break; 
 		}
-        pop(&path_to_meet_s);
-        pop(&path_to_meet_e);
     }
     char *start_parent = peek(&path_to_meet_s);
     char *end_parent = peek(&path_to_meet_e);
-    printf("%s", "success\n");
-    /*print_list(path_to_meet_s);
-    printf("\n");
-    print_list(path_to_meet_e);*/
-	
-	printf("path should be: %s %s %s %s %s\n", ht_get(hashtable_parents_start, ht_get(hashtable_parents_start, start_parent)), ht_get(hashtable_parents_start, start_parent), "0001159", ht_get(hashtable_parents_end, "0001159"), ht_get(hashtable_parents_end, ht_get(hashtable_parents_end, end_parent)));
-
+    
 	reverse(&path_to_meet_s);
 	pop(&path_to_meet_s);
 	reverse(&path_to_meet_e);
+	pop(&path_to_meet_e);
 	
 	char *parent_st = ht_get(hashtable_parents_start, start_parent);
 	char *parent_en = ht_get(hashtable_parents_end, end_parent);
@@ -166,6 +147,7 @@ int main(int argc, char **argv) {
 	push(&path_to_meet_s, parent_st);
 	reverse(&path_to_meet_s);
 	push(&path_to_meet_e, parent_en);
+	pop(&path_to_meet_e);
 	print_list(path_to_meet_s);
 	print_list(path_to_meet_e);
 		
